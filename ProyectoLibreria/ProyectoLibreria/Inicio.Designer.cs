2470e9f42ec97df344defd28d2f6549423a8d9cf
AlexAparic <141753383+AlexAparic@users.noreply.github.com>
2023-10-11 08:43:26 -0600

    Add files via upload

---
+++ ProyectoLibreria/ProyectoLibreria/Inicio.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace ProyectoLibreria
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInicioUsuarios = new FontAwesome.Sharp.IconButton();
+            this.btnInicioProveedor = new FontAwesome.Sharp.IconButton();
+            this.btnInicioInventario = new FontAwesome.Sharp.IconButton();
+            this.btnInicioVentas = new FontAwesome.Sharp.IconButton();
+            this.btnInicioCompras = new FontAwesome.Sharp.IconButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1064, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Inicio Libreria";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnInicioUsuarios
+            // 
+            this.btnInicioUsuarios.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnInicioUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioUsuarios.IconChar = FontAwesome.Sharp.IconChar.UserGear;
+            this.btnInicioUsuarios.IconColor = System.Drawing.Color.Black;
+            this.btnInicioUsuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnInicioUsuarios.IconSize = 70;
+            this.btnInicioUsuarios.Location = new System.Drawing.Point(6, 65);
+            this.btnInicioUsuarios.Name = "btnInicioUsuarios";
+            this.btnInicioUsuarios.Size = new System.Drawing.Size(183, 135);
+            this.btnInicioUsuarios.TabIndex = 1;
+            this.btnInicioUsuarios.Text = "Usuarios";
+            this.btnInicioUsuarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicioUsuarios.UseVisualStyleBackColor = false;
+            // 
+            // btnInicioProveedor
+            // 
+            this.btnInicioProveedor.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnInicioProveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioProveedor.IconChar = FontAwesome.Sharp.IconChar.PlaneDeparture;
+            this.btnInicioProveedor.IconColor = System.Drawing.Color.Black;
+            this.btnInicioProveedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnInicioProveedor.IconSize = 70;
+            this.btnInicioProveedor.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnInicioProveedor.Location = new System.Drawing.Point(182, 65);
+            this.btnInicioProveedor.Name = "btnInicioProveedor";
+            this.btnInicioProveedor.Size = new System.Drawing.Size(203, 135);
+            this.btnInicioProveedor.TabIndex = 2;
+            this.btnInicioProveedor.Text = "Proveedores";
+            this.btnInicioProveedor.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicioProveedor.UseVisualStyleBackColor = false;
+            this.btnInicioProveedor.Click += new System.EventHandler(this.btnInicioProveedor_Click);
+            // 
+            // btnInicioInventario
+            // 
+            this.btnInicioInventario.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnInicioInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioInventario.IconChar = FontAwesome.Sharp.IconChar.Inbox;
+            this.btnInicioInventario.IconColor = System.Drawing.Color.Black;
+            this.btnInicioInventario.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnInicioInventario.IconSize = 70;
+            this.btnInicioInventario.Location = new System.Drawing.Point(400, 62);
+            this.btnInicioInventario.Name = "btnInicioInventario";
+            this.btnInicioInventario.Size = new System.Drawing.Size(206, 138);
+            this.btnInicioInventario.TabIndex = 3;
+            this.btnInicioInventario.Text = "Inventario";
+            this.btnInicioInventario.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicioInventario.UseVisualStyleBackColor = false;
+            // 
+            // btnInicioVentas
+            // 
+            this.btnInicioVentas.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnInicioVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioVentas.IconChar = FontAwesome.Sharp.IconChar.MoneyCheck;
+            this.btnInicioVentas.IconColor = System.Drawing.Color.Black;
+            this.btnInicioVentas.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnInicioVentas.IconSize = 70;
+            this.btnInicioVentas.Location = new System.Drawing.Point(612, 62);
+            this.btnInicioVentas.Name = "btnInicioVentas";
+            this.btnInicioVentas.Size = new System.Drawing.Size(230, 138);
+            this.btnInicioVentas.TabIndex = 4;
+            this.btnInicioVentas.Text = "Ventas";
+            this.btnInicioVentas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicioVentas.UseVisualStyleBackColor = false;
+            // 
+            // btnInicioCompras
+            // 
+            this.btnInicioCompras.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnInicioCompras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioCompras.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.btnInicioCompras.IconColor = System.Drawing.Color.Black;
+            this.btnInicioCompras.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnInicioCompras.IconSize = 70;
+            this.btnInicioCompras.Location = new System.Drawing.Point(848, 59);
+            this.btnInicioCompras.Name = "btnInicioCompras";
+            this.btnInicioCompras.Size = new System.Drawing.Size(217, 141);
+            this.btnInicioCompras.TabIndex = 6;
+            this.btnInicioCompras.Text = "Compras";
+            this.btnInicioCompras.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicioCompras.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(1, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1064, 138);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(1, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1064, 371);
+            this.label3.TabIndex = 8;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 569);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnInicioCompras);
+            this.Controls.Add(this.btnInicioVentas);
+            this.Controls.Add(this.btnInicioInventario);
+            this.Controls.Add(this.btnInicioProveedor);
+            this.Controls.Add(this.btnInicioUsuarios);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconButton btnInicioUsuarios;
+        private FontAwesome.Sharp.IconButton btnInicioProveedor;
+        private FontAwesome.Sharp.IconButton btnInicioInventario;
+        private FontAwesome.Sharp.IconButton btnInicioVentas;
+        private FontAwesome.Sharp.IconButton btnInicioCompras;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ProyectoLibreria/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿Select * from Usuario
+
+Select * from TipoDeUsuario
+
+insert into TipoDeUsuario(Descripcion)
+values('ADMINISTRADOR')
+
+insert into Usuario(NombreCompleto, Usuario, Password, IdTipoDeUsuario)
+
+values ('William Aparicio', 'Aparicio', '123', '1')
